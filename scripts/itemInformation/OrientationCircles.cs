a23285900d042ac96ffd5e46a23fd9c10cebae4c
Sander <sandermoolin@gmail.com>
2014-05-17 08:49:11 -0400

    Imported Vectrosity and started working with visual guides

---
+++ scripts/itemInformation/OrientationCircles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class OrientationCircles : MonoBehaviour {
+
+	Vector2[] segments = new Vector2[100];
+	VectorLine xzAxis;
+
+	private void Start ()
+	{
+		xzAxis = new VectorLine ("XZ Circle", segments, Color.green, null, 2f);
+		xzAxis.MakeCircle(Camera.main.WorldToScreenPoint(transform.position), 200f);
+		xzAxis.Draw();
+	}
+
+
+}
