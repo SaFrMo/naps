b6b8593207e98c7b1d3142ed51982559bc25f829
Sander <sandermoolin@gmail.com>
2014-05-19 21:40:31 -0400

    Image to .blend system working

--- _workers/image_to_blender.py
+++ _workers/image_to_blender.py
@@ -1,12 +1,18 @@
 from bpy import context
 import os
 import bpy
+from PIL import Image
+import random
 
 # where text guides are stored
 path = "C:/Unity/naps/Assets/_workers/text/"
 
+def average_color(r):
+    return (r[0] + r[1] + r[2])/ 3
+
 def convert_to_shadower(filename):
     # open the relevant file
+    """
     with open(path + filename) as f:
         while True:
             # read next character
@@ -23,9 +29,31 @@ def convert_to_shadower(filename):
                 print (c)
             # shadow (block created)
             else:
+                
                 bpy.ops.mesh.primitive_cube_add(radius=.5, location=coordinates, layers=layer)
             # go to the next space
             coordinates[0] += 1
+
+    """
+    img = Image.open(path + filename)
+    img_pixels = img.load()
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            # light (no block)
+            if average_color(img_pixels[x, y]) >= 95:
+                print (x)
+            # shadow (block created)
+            else:
+                # random Z coordinate
+                coordinates[2] = random.randint(-5, 5)
+                bpy.ops.mesh.primitive_cube_add(radius=.5, location=coordinates, layers=layer)
+            # go to the next space
+            coordinates[0] += 1
+        coordinates[0] = 0
+        coordinates[1] += 1
+        
+            
+    
             
 
 coordinates = [0, 0, 0]
