b6efb17f897c6707e42ccb144ec0f6aea8c01563
Sander <sandermoolin@gmail.com>
2014-05-21 22:19:30 -0400

    Started Z axis elements

--- scripts/player/TurnOnShadow.cs
+++ scripts/player/TurnOnShadow.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class TurnOnShadow : MonoBehaviour {
 
+	public AudioClip pop;
+
 	private void OnCollisionEnter (Collision c)
 	{
 		if (!c.collider.GetComponent<MeshRenderer>().castShadows)
 		{
 			c.collider.GetComponent<MeshRenderer>().castShadows = true;
+			GetComponent<AudioSource>().PlayOneShot(pop);
 		}
 	}
 }
---
+++ scripts/player/ZLayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZLayerMovement : MonoBehaviour {
+
+	public float zMovementRate = 1f;
+	public float maxZ = 5f;
+	public float minZ = 5f;
+	private float currentZ;
+	private float targetZ;
+	public KeyCode zForwardKey = KeyCode.W;
+	public KeyCode zBackKey = KeyCode.S;
+
+	private void Start ()
+	{
+		currentZ = transform.position.z;
+		maxZ = currentZ + maxZ;
+		minZ = currentZ - minZ;
+		targetZ = currentZ;
+	}
+
+	private void Update ()
+	{
+		if (Input.GetKeyDown (zForwardKey) && targetZ < maxZ)
+		{
+			targetZ++;
+		}
+		if (Input.GetKeyDown (zBackKey) && targetZ > minZ)
+		{
+			targetZ--;
+		}
+
+		if (transform.position.z != targetZ)
+		{
+			transform.position = Vector3.Lerp (transform.position, 
+			                                   new Vector3 (transform.position.x, transform.position.y, targetZ),
+			                                   zMovementRate * Time.deltaTime);
+		}
+	}
+
+}
